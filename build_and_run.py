804cba007f3d2a5084faae1d983c13445afcceb1
elliird <irdali1996@gmail.com>
2023-12-08 20:58:26 +0900

    Made a Code that when given a window builds the dataset runs the model and saves it in the tar file as dict

---
+++ build_and_run.py
@@ -0,0 +1,10 @@
+import sys
+from pathlib import Path
+
+args = sys.argv
+
+
+if __name__ == '__main__'
+if len(args)!=1:
+    print("Wrong number of arguments, please specify the window size in seconds")
+    sys.exit()
